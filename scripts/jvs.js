173d56e22c6d613b1e68e70f156074b7c7272e0f
xeqtr87 <alidaman6@hotmail.com>
2015-09-27 20:40:21 +0000

    uppg 2

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -9,3 +9,12 @@ function medel (mt1, mt2, mt3, mt4)
 {
     console.log(medel(1,2,3,4));
 }
+
+function apa (n1, n2)
+{
+   
+    return n1 + ", " + n2;
+}
+console.log(apa("giraff","noshörning"));
+console.log(apa("idiot","åsna"));
+console.log(apa("titanic","clown"));
