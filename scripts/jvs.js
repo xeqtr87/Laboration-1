9ea8574f337ef568aac821b8665287cea8c029ec
xeqtr87 <alidaman6@hotmail.com>
2015-09-27 21:48:00 +0000

    added guessDNumber

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -18,3 +18,24 @@ function apa (n1, n2)
 console.log(apa("giraff","noshörning"));
 console.log(apa("idiot","åsna"));
 console.log(apa("titanic","clown"));
+
+var theX = Math.floor( Math.random() * 101)+1;
+{
+    console.log(theX);
+}
+var gissaDNumber = prompt ("Gissa numret!")
+
+
+
+if (gissaDNumber < theX)
+{
+    alert("fail, number " +gissaDNumber+ " is too low");
+}
+else if (gissaDNumber > theX)
+{
+    alert("fail, number "  +gissaDNumber+ " is to high");
+}
+else
+{
+    alert("GZ, the number " +gissaDNumber+ " is correct! You won the internet!")
+}
