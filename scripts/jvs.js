3a6fa0abc2e6150c43560f687c47c1f8681f7d38
xeqtr87 <alidaman6@hotmail.com>
2015-09-27 18:48:57 +0000

    javascript start

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -1,2 +1,11 @@
 
-console.log(typeof "50");
+console.log("Hello World");
+alert("Rad1\nRad2");
+
+function medel (mt1, mt2, mt3, mt4)
+{
+    return((mt1+mt2+mt3+mt4)/4);
+}
+{
+    console.log(medel(1,2,3,4));
+}
