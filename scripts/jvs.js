41927b158fa4393db14ccd0cbed6d8dae57a44ac
xeqtr87 <alidaman6@hotmail.com>
2015-09-28 16:37:13 +0000

    guess the number ok

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -1,55 +1,39 @@
-
 console.log("Hello World");
 alert("Rad1\nRad2");
 
-function medel (mt1, mt2, mt3, mt4)
-{
-    return((mt1+mt2+mt3+mt4)/4);
-}
-{
-    console.log(medel(1,2,3,4));
+function medel(mt1, mt2, mt3, mt4) {
+    return ((mt1 + mt2 + mt3 + mt4) / 4);
+} {
+    console.log(medel(1, 2, 3, 4));
 }
 
-function apa (n1, n2)
-{
-   
+function apa(n1, n2) {
+
     return n1 + ", " + n2;
 }
-console.log(apa("giraff","noshörning"));
-console.log(apa("idiot","åsna"));
-console.log(apa("titanic","clown"));
+console.log(apa("giraff", "noshörning"));
+console.log(apa("idiot", "åsna"));
+console.log(apa("titanic", "clown"));
 
+// Uppgift 3
 
-
-
-
-
-var loop=true
-var theX = Math.floor( Math.random() * 101)+1;
-{
+var loop = true
+var theX = Math.floor(Math.random() * 101) + 1; {
     console.log(theX);
 }
+var gissaDNumber = prompt("Gissa på ett nummer mellan 1 - 100!");
 
-while (loop){                   
-var gissaDNumber = prompt ("Gissa ett nummer mellan 1 - 100!");
-    if(gissaDNumber == theX){
-    alert("Rätt");
-    loop=false;
-    }
-    
-    if( gissaDNumber > theX){
-    prompt("Your number " +gissaDNumber+ " is to high try again")
+while (loop) {
+
+    if (gissaDNumber === theX.toString()) {
+        alert("Rätt");
+        loop = false;
         
     }
-    
-    if(gissaDNumber < theX){
-    prompt("Your number " +gissaDNumber+ " is to low try again")
-        
+    else if (gissaDNumber > theX) {
+        gissaDNumber = prompt("Your number " + gissaDNumber + " is to high, try again my good sir")
     }
-} 
-
-
-    
-    
-    
-
+    else if (gissaDNumber < theX) {
+        gissaDNumber = prompt("Your number " + gissaDNumber + " is to low, try again my good sir")
+    }
+}
