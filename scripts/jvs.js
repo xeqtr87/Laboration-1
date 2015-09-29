e3d5d09e0965b625337750e8414de1be842dac27
xeqtr87 <alidaman6@hotmail.com>
2015-09-29 08:33:41 +0000

    Added menu

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -15,25 +15,87 @@ console.log(apa("giraff", "noshörning"));
 console.log(apa("idiot", "åsna"));
 console.log(apa("titanic", "clown"));
 
-// Uppgift 3
 
-var loop = true
-var theX = Math.floor(Math.random() * 101) + 1; {
-    console.log(theX);
+
+
+// Uppgift 4
+
+var sten = true
+while (sten) {
+    var meny = prompt("0. Exit\n\n1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit\n3. Guess the number");
+    switch (meny)
+    {
+        case '0':
+            console.log("You have chosen Exit");
+            sten = false
+            break;
+        case '1':
+            var fahrenheit = prompt("Fahrenheit to Celsius\n\nEnter the degree in celsius plz")
+            alert(kamel(fahrenheit));
+            console.log("You have chosen Fah..");
+            break;
+        case '2':
+            var celsius = prompt("Celsius to Fahrenheit\n\nEnter the degree in fahrenheit plz")
+            alert(lama(celsius));
+            console.log("You have chosen Cel..");
+            break;
+        case '3':
+            var gissaDNumber = prompt("Gissa på ett nummer mellan 1 - 100!");
+            guessNumber();
+            break;
+        default:
+            console.log("Chose wisely");
+    }
+    
 }
-var gissaDNumber = prompt("Gissa på ett nummer mellan 1 - 100!");
 
-while (loop) {
+//Moment Fahrenheit & Celsius
 
-    if (gissaDNumber === theX.toString()) {
-        alert("Rätt");
-        loop = false;
-        
+function kamel(fahrenheit) {
+    var fahrAnswer = (fahrenheit * (9 / 5) + 32);
+
+    console.log(fahrAnswer);
+    return fahrAnswer;
+}
+
+function lama(celsius) {
+    var celAnswer = ((celsius - 32) * (5 / 9));
+
+    console.log(celAnswer.toFixed(2));
+    return celAnswer.toFixed(2);
+}
+
+
+
+
+
+
+
+
+// Uppgift 3
+function guessNumber() {
+
+    var count = 1;
+    var loop = true
+    var theX = Math.floor(Math.random() * 101) + 1; {
+        console.log(theX);
     }
-    else if (gissaDNumber > theX) {
-        gissaDNumber = prompt("Your number " + gissaDNumber + " is to high, try again my good sir")
-    }
-    else if (gissaDNumber < theX) {
-        gissaDNumber = prompt("Your number " + gissaDNumber + " is to low, try again my good sir")
+
+
+    while (loop) {
+        console.log(count);
+
+        if (gissaDNumber === theX.toString()) {
+            alert("My man! You guessed correct after " + count + " tries, noob!");
+            loop = false;
+
+        }
+        else if (gissaDNumber > theX) {
+            gissaDNumber = prompt("Your number " + gissaDNumber + " is to high, try again my good sir!")
+        }
+        else if (gissaDNumber < theX) {
+            gissaDNumber = prompt("Your number " + gissaDNumber + " is to low, try again my good sir!")
+        }
+        count++;
     }
 }
