8e3b228474bd7f40fef3e888376290eba06af1a7
xeqtr87 <alidaman6@hotmail.com>
2015-10-01 14:01:25 +0000

    Labb 1 -ok

--- scripts/jvs.js
+++ scripts/jvs.js
@@ -1,30 +1,15 @@
-/*console.log("Hello World");
-alert("Rad1\nRad2");
-
-function medel(mt1, mt2, mt3, mt4) {
-    return ((mt1 + mt2 + mt3 + mt4) / 4);
-} {
-    console.log(medel(1, 2, 3, 4));
-}
-
-function apa(n1, n2) {
-
-    return n1 + ", " + n2;
-}
-console.log(apa("giraff", "noshörning"));
-console.log(apa("idiot", "åsna"));
-console.log(apa("titanic", "clown"));
 
 
 
 
-// Uppgift 4
 
+menu();
+function menu()
+{
 var sten = true
 while (sten) {
-    var meny = prompt("0. Exit\n\n1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit\n3. Guess the number\n4. Convert to capitol");
-    switch (meny)
-    {
+    var meny = prompt("0. Exit\n\n1. Fahrenheit to Celsius\n2. Celsius to Fahrenheit\n3. Guess the number\n4. Uppgift 1\n5. Uppgift 2 moment 1\n6. Uppgift 2 moment 2\n7. Uppgift 5\n8. Uppgift 6\n9. Uppgift 7\n10. Uppgift 8");
+    switch (meny) {
         case '0':
             console.log("You have chosen Exit");
             sten = false
@@ -32,235 +17,255 @@ while (sten) {
         case '1':
             var fahrenheit = prompt("Fahrenheit to Celsius\n\nEnter the degree in celsius plz")
             alert(kamel(fahrenheit));
-            console.log("You have chosen Fah..");
             break;
         case '2':
             var celsius = prompt("Celsius to Fahrenheit\n\nEnter the degree in fahrenheit plz")
             alert(lama(celsius));
-            console.log("You have chosen Cel..");
             break;
         case '3':
             var gissaDNumber = prompt("Gissa på ett nummer mellan 1 - 100!");
-            guessNumber();
+            guessNumber(gissaDNumber);
+            break;
+        case '4':
+            welcome();
+            break;
+        case '5':
+            console.log(medel(1, 2, 3, 4));
+            break;
+        case '6':
+            console.log(showArgument("giraff", "noshörning"));
+            console.log(showArgument("idiot", "åsna"));
+            console.log(showArgument("titanic", "clown"));
+            break;
+        case '7':
+            var TypeText = prompt("Skriv en text")
+            InOut(TypeText);
+            break;
+        case '8':
+            var aSquare = prompt("Ange längden på första katetetern")
+            var bSquare = prompt("Ange längden på andra katetern")
+            triangel(aSquare, bSquare);
+            break;
+        case '9':
+            var nyvariabel = tentamen()
+            console.log(nyvariabel);
+            break;
+        case '10':
+            var month = prompt("Vilken månad fyller du år ? (1-12)")
+            var userday = prompt("Vilken dag fyller du år ? (1-31)")
+            myBirthday(userday, month);
             break;
             default:
             console.log("Chose wisely");
+
+
     }
+
+}
+}
+//uppgift 1
+function welcome() {
+    console.log("Hello World");
+    alert("Rad1\nRad2");
+}
+
+
+//uppgift 2
+function medel(mt1, mt2, mt3, mt4) {
+    return ((mt1 + mt2 + mt3 + mt4) / 4);
+
     
 }
 
+//uppgift 2 moment 2
+function showArgument(n1, n2) {
+
+    return n1 + ", " + n2;
+}
+
+
 //Moment Fahrenheit & Celsius
-{
 
-function kamel(fahrenheit) {
-    var fahrAnswer =  (fahrenheit * (9 / 5) + 32);
 
-    console.log(Math.round(fahrAnswer));
-    return Math.round(fahrAnswer);
-}
+    function kamel(fahrenheit) {
+        var fahrAnswer = (fahrenheit * (9 / 5) + 32);
 
-function lama(celsius) {
-    var celAnswer = ((celsius - 32) * (5 / 9));
+        console.log(Math.round(fahrAnswer));
+        return Math.round(fahrAnswer);
+    }
 
-    console.log(Math.round(celAnswer.toFixed(2)));
-    return Math.round(celAnswer.toFixed(2));
-}
-}
+    function lama(celsius) {
+        var celAnswer = ((celsius - 32) * (5 / 9));
+
+        console.log(Math.round(celAnswer.toFixed(2)));
+        return Math.round(celAnswer.toFixed(2));
+    }
+
 // Uppgift 3
-{
-function guessNumber() {
 
-    var count = 1;
-    var loop = true
-    var theX = Math.floor(Math.random() * 101) + 1; {
-        console.log(theX);
+    function guessNumber(gissaDNumber) {
+
+        var count = 1;
+        var loop = true
+        var theX = Math.floor(Math.random() * 101) + 1; {
+            console.log(theX);
+        }
+
+
+        while (loop) {
+            console.log(count);
+
+            if (gissaDNumber === theX.toString()) {
+                alert("My man! You guessed correct after " + count + " tries, noob!");
+                loop = false;
+
+            }
+            else if (gissaDNumber > theX) {
+                gissaDNumber = prompt("Your number " + gissaDNumber + " is to high, try again my good sir!")
+            }
+            else if (gissaDNumber < theX) {
+                gissaDNumber = prompt("Your number " + gissaDNumber + " is to low, try again my good sir!")
+            }
+            count++;
+        }
     }
 
 
-    while (loop) {
-        console.log(count);
-
-        if (gissaDNumber === theX.toString()) {
-            alert("My man! You guessed correct after " + count + " tries, noob!");
-            loop = false;
-
-        }
-        else if (gissaDNumber > theX) {
-            gissaDNumber = prompt("Your number " + gissaDNumber + " is to high, try again my good sir!")
-        }
-        else if (gissaDNumber < theX) {
-            gissaDNumber = prompt("Your number " + gissaDNumber + " is to low, try again my good sir!")
-        }
-        count++;
-    }
-}
-}
 
 
 
 
 
-*/
 
 //Uppgift 5
 
 
-/*
-var TypeText = prompt("Skriv en text")
-var HashT = ""
-for (var i = 0; i < TypeText.length; i++) {
-    //    console.log("The character at " +i+ " is " + TypeText.charAt(i) );
-    if (TypeText[i].toUpperCase() === 'A') {
-        HashT += "#"
+function InOut(TypeText) {
+    var HashT = ""
+    for (var i = 0; i < TypeText.length; i++) {
+
+        if (TypeText[i].toUpperCase() === 'A') {
+            HashT += "#"
+        }
+        else if (TypeText[i] === TypeText[i].toUpperCase()) {
+            HashT += TypeText[i].toLowerCase()
+        }
+        else if (TypeText[i] === TypeText[i].toLowerCase()) {
+            HashT += TypeText[i].toUpperCase()
+        }
+        else {
+            HashT += TypeText[i]
+        }
     }
-    else if (TypeText[i] === TypeText[i].toUpperCase())
-    {
-        HashT += TypeText[i].toLowerCase()
-    }
-    else if (TypeText[i] === TypeText[i].toLowerCase())
-    {
-        HashT += TypeText[i].toUpperCase()
-    }
-    else {
-        HashT += TypeText[i]
-    }
-}
-console.log(HashT);
-
-*/
-
-//Uppgift 6
-/*
-function triangel ()
-{
-var aSquare = prompt("Ange längden på första katetetern")
-var bSquare = prompt("Ange längden på andra katetern")
-var aAndb = Math.pow(aSquare, 2) + Math.pow(bSquare, 2);
-
-{
-console.log(aAndb);
-console.log(Math.round(Math.sqrt(aAndb)));
-
-}
-}
-*/
-
-//uppgift 7
-/*
-console.log(tentamen());
-
-function tentamen() {
-    var medelV = 0;
-    var myArray = [10,2,89,9,65,13,3];
-    console.log(myArray); 
-    for (var i = 0; i < myArray.length; i++) {
-        medelV += myArray[i];
-    }
-    var summa = (medelV / myArray.length)
-    console.log(summa)
-    myArray = myArray.sort(function(a, z) {
-        return a - z;
-    })
-    var minimum = myArray[0]
-    var maximum = myArray[myArray.length - 1]
-    var myFinalArray = [Math.round(summa), maximum, minimum]
-    return myFinalArray;
-}
-*/
-/*
-
-
-
-//månad
-var monthQ=true
-
-while (monthQ) 
-{
-var month = prompt ("Vilken månad fyller du år ?(1-12)")
-
-    if (month <= 12)
-    {
-    console.log(month);
-    monthQ=false;
-    }
-   
-
+    console.log(HashT);
 }
 
-var userday = prompt ("Vilken dag fyller du år")
-console.log(userday);
-
-var usermonth = (month-1);
-console.log(usermonth);
-
-//1 dag i millisekunder
-var oneDay=(1000*60*60*24)
-console.log(oneDay)
-
-//Millisecs uptodate
-var todayMilli = Date.now();
-console.log(todayMilli+ " angivet datum i millisekunder")
 
 
+    //Uppgift 6
 
-//dagens datum
-var today = new Date();
-console.log(today);
+    function triangel(aSquare, bSquare) {
 
-//nuvarande månad
-var thismonth = today.getUTCMonth();
+        var aAndb = Math.pow(aSquare, 2) + Math.pow(bSquare, 2);
 
-var year=""
+        {
+            console.log(aAndb);
+            console.log(Math.round(Math.sqrt(aAndb)));
 
-if (thismonth >= usermonth) {
+        }
+    }
+
+
+    //uppgift 7
+
     
-    // Oktober 
-    if (today > userday) {
-        alert("året är 2016")
-        year=today.getFullYear()+1
+
+    function tentamen() {
+        var medelV = 0;
+        var myArray = [10, 2, 89, 9, 65, 13, 3];
+       
+        for (var i = 0; i < myArray.length; i++) {
+            medelV += myArray[i];
+        }
+        var summa = (medelV / myArray.length)
+      
+        myArray = myArray.sort(function(a, z) {
+            return a - z;
+        })
+      
+        var minimum = myArray[0]
+        var maximum = myArray[myArray.length - 1]
+        var myFinalArray = [Math.round(summa), maximum, minimum]
+        return myFinalArray;
     }
-    else
-    {
-        alert("året är 2015")
-        year=today.getFullYear()
+
+
+
+
+    //månad
+    function myBirthday(userday, month) {
+            if (month > 12) {
+                console.log(month);
+                menu();
+            }
+                if (userday > 31) {
+                console.log(userday);
+                menu();
+            }
+        
+
+        var usermonth = (month - 1);
+        console.log(usermonth);
+
+        //1 dag i millisekunder
+        var oneDay = (1000 * 60 * 60 * 24)
+        console.log(oneDay)
+
+        //Millisecs uptodate
+        var todayMilli = Date.now();
+        console.log(todayMilli + " angivet datum i millisekunder")
+
+
+
+        //dagens datum
+        var today = new Date();
+        console.log(today);
+
+        //nuvarande månad
+        var thismonth = today.getUTCMonth();
+
+        var year = ""
+
+        if (thismonth >= usermonth) {
+
+            // Oktober 
+            if (today > userday) {
+                alert("året är 2016")
+                year = today.getFullYear() + 1
+            }
+            else {
+                alert("året är 2015")
+                year = today.getFullYear()
+            }
+
+        }
+        else {
+            alert("året är 2015")
+            year = today.getFullYear()
+        }
+        var birthday = new Date(year, usermonth, userday);
+        console.log(birthday);
+
+        var Bday = Date.parse(birthday)
+        console.log(Bday + " födelsedagen")
+
+        var mellanskillnad = Bday - todayMilli
+        console.log(mellanskillnad + " mellanskillnad")
+
+        var convertion = mellanskillnad / oneDay
+        console.log(Math.round(convertion));
+
+        var copy = new Date();
+        copy.setTime(birthday.getTime());
+        console.log(copy);
     }
-    
-}
-else {
-    alert("året är 2015")
-    year=today.getFullYear()
-}
-var birthday = new Date(year ,usermonth, userday);
-console.log(birthday);
-
-var Bday = Date.parse(birthday)
-console.log(Bday+ " födelsedagen")
-
-var mellanskillnad = Bday - todayMilli
-console.log(mellanskillnad+ " mellanskillnad")
-
-var convertion = mellanskillnad/oneDay
-console.log(Math.round(convertion));
-
-var copy = new Date();
-copy.setTime(birthday.getTime());
-console.log(copy);
-
-
-newContent()
-function newContent()
-{
-alert("load new content");
-document.open();
-document.write("<h1>Out with the old - in with the new!</h1>");
-document.close();
-}
-*/
-
-
-
-
-
-
-
-
